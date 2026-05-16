d7d088260fe19e418f44f6f592c721110d769b8d
acidbull <99673727+acidbull@users.noreply.github.com>
2026-05-16 20:14:33 +0300

    Update QAM.cpp

--- QAM.cpp
+++ QAM.cpp
@@ -223,7 +223,7 @@ void runSimulation(QAMModulator::Type modType,
 
 int main() {
 
-    const int numBits = 200000; 
+    const int numBits = 200004; 
     
     std::vector<double> variances;
     for (double snr_db = 20; snr_db >= 0; snr_db -= 1.0) {
@@ -240,15 +240,18 @@ int main() {
 
     qpsk = np.loadtxt('ber_qpsk.txt', comments='#')
     qam16 = np.loadtxt('ber_qam16.txt', comments='#')
+    qam64 = np.loadtxt('ber_qam64.txt', comments='#')
 
     qpsk = qpsk[qpsk[:,1] > 0]
 
     snr_qpsk = 10 * np.log10(1.0 / qpsk[:,0])
     snr_qam16 = 10 * np.log10(1.0 / qam16[:,0])
+    snr_qam64 = 10 * np.log10(1.0 / qam64[:,0])
 
     plt.figure(figsize=(10, 6))
     plt.semilogy(snr_qpsk, qpsk[:,1], 'o-', label='QPSK')
     plt.semilogy(snr_qam16, qam16[:,1], 's-', label='16-QAM')
+    plt.semilogy(snr_qam64, qam64[:,1], 's-', label='64-QAM')
     plt.xlabel('SNR (dB)')
     plt.ylabel('Bit Error Rate (BER)')
     plt.title('BER и SNR для QAM Модуляции')
